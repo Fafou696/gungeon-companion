c12ad8aa65380e937d071791b2ab4c470c234cc3
Fafou696 <marquet.fabrice@me.com>
2026-07-13 19:00:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-const CACHE='gungeon-companion-v7-1';
-const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data/guide.json','./data/floors.json'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
-});
+const CACHE='gungeon-companion-v8';
+const CORE=['./','./index.html','./styles.css','./app.js','./pictograms.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data/guide.json','./data/floors.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
 ));
 self.addEventListener('fetch',e=>e.respondWith(
-  fetch(e.request).then(r=>{
-    const copy=r.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,copy));
-    return r;
-  }).catch(()=>caches.match(e.request))
+  fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r})
+  .catch(()=>caches.match(e.request))
 ));
