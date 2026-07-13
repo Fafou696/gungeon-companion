f1b37b1b74b879917836d5a8686860b71e67e3d9
Fafou696 <marquet.fabrice@me.com>
2026-07-13 17:41:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-const CACHE='gungeon-companion-v3';
+const CACHE='gungeon-companion-v4';
 const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
+  e.respondWith(fetch(e.request).then(r=>{
+    const c=r.clone();
+    caches.open(CACHE).then(x=>x.put(e.request,c));
+    return r;
+  }).catch(()=>caches.match(e.request)));
 });
