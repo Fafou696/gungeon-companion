0d321559a1e03d9f4a738ee8bb607c14c980af61
Fafou696 <marquet.fabrice@me.com>
2026-07-13 17:56:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-const CACHE='gungeon-companion-v5';
-const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='gungeon-companion-v6';
+const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./data/guide.json','./data/floors.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).then(r=>{
-    const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;
-  }).catch(()=>caches.match(e.request)));
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
 });
